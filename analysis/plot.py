3e6eec92d6d7b1f0a580ee9ad58a039d489ebf9c
Jackson Dean <Jackson.Dean@uvm.edu>
2023-11-29 15:04:46 -0500

    Improve plotting

--- analysis/plot.py
+++ analysis/plot.py
@@ -4,68 +4,138 @@ import os
 import seaborn as sns
 import matplotlib.pyplot as plt
 import torch
+from tqdm import tqdm
+import multiprocessing as mp
 
-def read_results(results_path):
-    fits_path = os.path.join(results_path, "results.pkl")
-    df = pd.read_pickle(fits_path)
-    return df
+     
+def process_tensor(result, name):
+    if name in ['offspring_by_batch', 'pruned_cxs']:
+        return result
+    if name in ['lr_by_batch']:
+        return result.mean(dim=0)
+    if name in ["fitness_by_batch", "normed_fitness_by_batch"]:
+        return result.nanmean(dim=(0,1))
+    if name in ['evals_by_batch']:
+        fwds_sgd = result[:,1] # only n_fwds_incl_sgd
+        # cumlative sum:
+        fwds_sgd = fwds_sgd.cumsum(dim=0)
+        return fwds_sgd
+    if name in ['nodes_by_batch', 'cx_by_batch']:
+        return result[:,1] # average over pop
 
-def read_tensor_results(results_path, name):
+
+def read_tensor_results(results_path, names, max_runs=None):
     cond_dir = os.path.join(results_path, "conditions")
     results = []
-    for cond in os.listdir(cond_dir):
-        cond_path = os.path.join(cond_dir, cond)
-        if not os.path.isdir(cond_path):
-            continue
-        for run in os.listdir(cond_path):
-            pt_path = os.path.join(cond_path, run, f"{name}.pt")
-            pt_off_path = os.path.join(cond_path, run, f"offspring_over_time.pt")
-            if not os.path.exists(pt_path):
+    for name in tqdm(names, desc="Loading"):
+        df_index_offset = 0 
+        for cond in os.listdir(cond_dir):
+            cond_path = os.path.join(cond_dir, cond)
+            if not os.path.isdir(cond_path):
                 continue
-            with open(os.path.join(cond_path, run, "target.txt"), 'r') as f:
-                target = f.read().strip()
-            df = pd.DataFrame(torch.load(pt_path).numpy(), columns=[name])
-            df["condition"] = cond
-            df["run"] = str(run)
-            df["target"] = target
-            df["gen"] = df.index
-            df["total_offspring"] = torch.load(pt_off_path).numpy()
-            results.append(df)
-    return pd.concat(results)
+            runs = os.listdir(cond_path)[:max_runs] if max_runs is not None else os.listdir(cond_path)
+            for run in runs:
+                pt_path = os.path.join(cond_path, run, f"{name}.pt")
+                if not os.path.exists(pt_path):
+                    continue
+                t = torch.load(pt_path)
+                t = process_tensor(t, name)
+                if t is None:
+                    print("Did not find tensor for", name, "at", pt_path)
+                    continue
+                
+                with open(os.path.join(cond_path, run, "target.txt"), 'r') as f:
+                    target = f.read().strip()
+                df = pd.DataFrame(t.numpy(), columns=[name])
+                df["condition"] = cond
+                df["run"] = str(run)
+                df["target"] = target
+                df["batch"] = df.index
+                
+                df = df[df[name].notna()]
+                df = df[df[name] != float('inf')]
+                df = df[df[name] != float('-inf')]
+                df = df[df[name] != float('nan')]
+                df = df[df[name] != float('NaN')]
+                df = df[df[name] != 'NaN']
+                df = df.reset_index(drop=True)
+                
+                
+                df.index = df.index + df_index_offset
+                # print(df)
+                df_index_offset += len(df)
+                results.append(df)
+    
+    print(len(results))
+    final_df = results[0]
+    for df in tqdm(results[1:], desc="Merging"):
+        final_df = final_df.combine_first(df)
+    return final_df
 
-def plot_vs_gens(results, y, save_path=None, show=False, max_ofs=None):
-    if max_ofs is not None:
-        results = results[(results["total_offspring"] <= max_ofs) & (results["total_offspring"] > 0)]
-    sns.lineplot(results, x="gen", y=y, hue="condition", style="target")
+
+
+def plot_xy(results, x, y, save_path=None, show=False, x_label=None, y_label=None):
+    plt.figure(figsize=(10,10))
+    use_save_path = os.path.join(save_path, f"{y}_v_batches.png")
+    if 'target' not in results.columns:
+        sns.lineplot(results, x=x, y=y, hue="condition")
+        use_save_path = use_save_path.replace(".png", "_avg.png")
+    else:
+        sns.lineplot(results, x=x, y=y, hue="condition", style="target")
+    
+    plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+    if x_label is not None:
+        plt.xlabel(x_label)
+    if y_label is not None:
+        plt.ylabel(y_label)
+    
+    # plt.tight_layout(rect=[0, 0, .8, 1])
     if save_path:
-        plt.savefig(os.path.join(save_path, f"{y}_v_gens.png"))
+        plt.savefig(use_save_path, bbox_inches='tight')
     if show:
         plt.show()
     plt.close()
+
+
+def plot_vs_batches(results, y, save_path=None, show=False, max_ofs=None):
+    if max_ofs is not None:
+        results = results[(results["offspring_by_batch"] <= max_ofs) & (results["offspring_by_batch"] > 0)]
+    plot_xy(results, "batch", y, save_path, show)    
+
+def plot_vs_evals(results, y, save_path=None, show=False, mean_by_target=False):
+    results = results.drop(columns=[c for c in results.columns if c not in[ "condition", "target", 'run', 'batch', 'evals_by_batch', y]])
+    if mean_by_target:
+        results = results.groupby(["condition", 'run', 'batch']).mean(numeric_only=True).reset_index()
+        # take the mean evals_by_batch for each batch
+        results['evals_by_batch'] = results.groupby(["condition", "batch"])['evals_by_batch'].transform('mean')
+    else:
+        results = results.groupby(["condition", "target", 'run','batch']).mean(numeric_only=True).reset_index()
+        # take the mean evals_by_batch for each batch
+        results['evals_by_batch'] = results.groupby(["condition", 'target', "batch"])['evals_by_batch'].transform('mean')
+   
+   
+    plot_xy(results, "evals_by_batch", y, save_path, show, x_label="Forward passes")
+    
     
 def plot_vs_offspring(results, y, save_path=None, show=False, mean_by_target=False):
-    results = results.drop(columns=[c for c in results.columns if c not in[ "condition", "target", 'run', 'gen', 'total_offspring', y]])
+    results = results.drop(columns=[c for c in results.columns if c not in[ "condition", "target", 'run', 'batch', 'offspring_by_batch', y]])
     if mean_by_target:
-        results = results.groupby(["condition", 'run', 'gen']).mean(numeric_only=True).reset_index()
-        sns.lineplot(results, x="total_offspring", y=y, hue="condition")
+        results = results.groupby(["condition", 'run', 'batch']).mean(numeric_only=True).reset_index()
+        # save_path = save_path.split(".")[-2] + "_avg" + save_path.split(".")[-1]
     else:
-        results = results.groupby(["condition", "target", 'run','gen']).mean(numeric_only=True).reset_index()
-        sns.lineplot(results, x="total_offspring", y=y, hue="condition", style="target")
-    if save_path:
-        plt.savefig(os.path.join(save_path, f"{y}_v_offspring.png"))
-    if show:
-        plt.show()
-    plt.close()
+        results = results.groupby(["condition", "target", 'run','batch']).mean(numeric_only=True).reset_index()
+    
+    plot_xy(results, "offspring_by_batch", y, save_path, show)
     
 def plot_vs_time(results, y, save_path=None, show=False, mean_by_target=False):
-    results = results.groupby(["condition", "target", 'run', 'gen']).mean(numeric_only=True).reset_index()
-    # get the average time at each generation
-    results = results.groupby(["condition", "target", "gen"]).agg({"time": "mean", y:"mean"}).reset_index()
+    results = results.groupby(["condition", "target", 'run', 'batch']).mean(numeric_only=True).reset_index()
+    # get the average time at each batcheration
+    results = results.groupby(["condition", "target", "batch"]).agg({"time": "mean", y:"mean"}).reset_index()
     
     save_name = f"{y}_v_time.png"
     print(results)
     if mean_by_target:
-        results = results.groupby(["condition", "gen"]).agg({"time": "mean", y:"mean"}).reset_index()
+        results = results.groupby(["condition", "batch"]).agg({"time": "mean", y:"mean"}).reset_index()
         sns.lineplot(results, x="time", y=y, hue="condition")
         save_name = f"{y}_v_time_avg.png"
     else:
@@ -86,36 +156,23 @@ if __name__ == "__main__":
     
     save_path = os.path.join(args.results_path, "plots")
     os.makedirs(os.path.join(save_path), exist_ok=True)
-    
-    results = read_results(args.results_path)
-    # remove nan rows
-    results = results[results['condition'].notna()]
-    results['run'] = results['run'].astype(str)
-    assert len(args.target)==0  or args.target in results["target"].unique()
-    if len(args.target) > 0:
-        results = results[results["target"] == args.target]
 
-    plot_vs_time(results, "fitness", save_path, args.show)
-    plot_vs_time(results, "fitness", save_path, args.show, mean_by_target=True)
-    plot_vs_time(results, "total_offspring", save_path, args.show, mean_by_target=True)
-    plot_vs_offspring(results, "fitness", save_path, args.show, mean_by_target=True)
-    
-    
-    plot_vs_offspring(results, "avg_num_connections", save_path, args.show)
-    plot_vs_offspring(results, "avg_num_hidden_nodes", save_path, args.show)
-    plot_vs_offspring(results, "time", save_path, args.show)
-    plot_vs_offspring(results, "population", save_path, args.show)
-    plot_vs_offspring(results, "fitness", save_path, args.show)
-    # plot_vs_offspring(results, "diversity", save_path, args.show)
-    
-    
     metrics = [
-        "lr_over_time",
+        "evals_by_batch",
+        "normed_fitness_by_batch",
+        # "offspring_by_batch",
+        # "fitness_by_batch",
+        "cx_by_batch",
+        "nodes_by_batch",
+        "pruned_cxs",
         
     ]
-    offs_results = read_tensor_results(args.results_path, "offspring_over_time")
-    plot_vs_gens(offs_results, "offspring_over_time", save_path, args.show, results["total_offspring"].max())
+    # offs_results = read_tensor_results(args.results_path, "offspring_by_batch")
+    results = read_tensor_results(args.results_path, metrics, max_runs=None)
+    results = results.dropna()
+    print(results)
     
-    for m in metrics:
-        tensor_results = read_tensor_results(args.results_path, m)
-        plot_vs_offspring(tensor_results, m, save_path, args.show)
+    for m in tqdm(['normed_fitness_by_batch', 'cx_by_batch', 'nodes_by_batch', 'pruned_cxs'], desc="Plotting"):
+        plot_vs_evals(results, m, save_path, args.show)
+        plot_vs_evals(results, m, save_path, args.show, mean_by_target=True)
+ 
